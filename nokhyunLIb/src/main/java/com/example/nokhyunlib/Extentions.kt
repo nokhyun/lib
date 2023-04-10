56840ac591af1799eb1969b6293765f33d68bf58
kwangjun <kwangjun.cho@ikoob.com>
2023-04-10 14:12:34 +0900

    toast

--- nokhyunLIb/src/main/java/com/example/nokhyunlib/Extentions.kt
+++ nokhyunLIb/src/main/java/com/example/nokhyunlib/Extentions.kt
@@ -1,3 +1,13 @@
 package com.example.nokhyunlib
 
+import android.content.Context
+import android.widget.Toast
+
 val test = "abc"
+
+object AndroidKtx {
+
+    fun String.showToast(context: Context) {
+        Toast.makeText(context, this, Toast.LENGTH_SHORT).show()
+    }
+}

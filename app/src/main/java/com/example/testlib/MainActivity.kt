6ce55c073b17a9afdd37d4a591dad1b0f45e5587
nokhyun90 <nokhyun90@gmail.com>
2023-04-11 23:16:00 +0900

    snackbar

--- app/src/main/java/com/example/testlib/MainActivity.kt
+++ app/src/main/java/com/example/testlib/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.testlib
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.example.nokhyunlib.showSnackbar
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        com.example.nokhyunlib.test
+
+        window?.decorView?.showSnackbar(
+            msg = "Hello World",
+            onDismiss = {
+                log("onDismiss")
+            }
+        )
+    }
+
+    private fun log(msg: String){
+        Log.e(this.javaClass.simpleName, msg)
     }
 }
--- nokhyunLIb/src/main/java/com/example/nokhyunlib/Extentions.kt
+++ nokhyunLIb/src/main/java/com/example/nokhyunlib/Extentions.kt
@@ -1,13 +1,36 @@
 package com.example.nokhyunlib
 
 import android.content.Context
+import android.view.View
 import android.widget.Toast
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
 
-val test = "abc"
-
-object AndroidKtx {
-
-    fun String.showToast(context: Context) {
-        Toast.makeText(context, this, Toast.LENGTH_SHORT).show()
-    }
+fun String.showToast(context: Context) {
+    Toast.makeText(context, this, Toast.LENGTH_SHORT).show()
 }
+
+fun View.showSnackbar(
+    msg: CharSequence,
+    duration: Int = 2000,
+    onShown: (() -> Unit)? = null,
+    onDismiss: (() -> Unit)? = null,
+    actionText: String? = null,
+    action: ((View) -> Unit)? = null
+) {
+    Snackbar.make(this, msg, duration)
+        .setAction(actionText, action)
+        .addCallback(object : BaseTransientBottomBar.BaseCallback<Snackbar>() {
+            override fun onShown(transientBottomBar: Snackbar?) {
+                super.onShown(transientBottomBar)
+                onShown?.invoke()
+            }
+
+            override fun onDismissed(transientBottomBar: Snackbar?, event: Int) {
+                super.onDismissed(transientBottomBar, event)
+                onDismiss?.invoke()
+            }
+        }).show()
+}
+
+
